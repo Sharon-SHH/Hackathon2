80bf0966b5b52dd952cca83cc89d20e9a3ed5d41
Sharon-SHH <sharonheng.shi@gmail.com>
2024-02-01 14:27:00 -0500

    function done

--- thebugbusters/src/App.js
+++ thebugbusters/src/App.js
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import Coloring from "./Coloring/Coloring";
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
-  const [colors, setColors] = useState(null);
-  
+  const [colors, setColors] = useState([]);
+  const url = "https://imagga.com/static/images/tagging/wind-farm-538576_640.jpg";
   const getData = async ()=> {
     try {
       console.log(REACT_APP_SERVER_URL);
-      const response = await axios.get(`${REACT_APP_SERVER_URL}/profile`);
-      console.log(response);
-
+      const response = await axios.get(`${REACT_APP_SERVER_URL}/colors?url=${url}`);
+      const tmpColors = response.data.result.colors.background_colors;
+      setColors(tmpColors);
+      console.log(tmpColors);
+      console.log(colors);
     } catch(error) {
       console.log(error);
     }
   }
+  
+  useEffect(() => {
+    getData();
+  }, [url]);
 
   return (
     <div className="App">
@@ -22,12 +29,7 @@ function App() {
         {/* new line start*/}
         <p>To get your profile details: </p>
         <button onClick={getData}>Click me</button>
-        {colors && (
-          <div>
-            <p>Color Name: {colors.color_name}</p>
-            <p>Color Parent: {colors.color_parent}</p>
-          </div>
-        )}
+        <Coloring img_url={url} colors={colors}/>
         {/* end of new line */}
       </header>
     </div>
