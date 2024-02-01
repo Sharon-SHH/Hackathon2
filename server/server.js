02a97d6934b4a4ac93fee894cc7e2cc29d3e502c
Sharon-SHH <sharonheng.shi@gmail.com>
2024-02-01 15:06:09 -0500

    server

---
+++ server/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+// const axios = require("axios");
+const express = require("express");
+
+
+const app = express();
+const cors = require("cors");
+const got = require("got"); // if you don't have "got" - install it with "npm install got"
+
+const apiKey = "acc_fffbff1d9c6a505";
+const apiSecret = "f9b8745ab46b564e82c57aacac1e83dc";
+
+const imageUrl =
+  "https://imagga.com/static/images/tagging/wind-farm-538576_640.jpg";
+const url =
+  "https://api.imagga.com/v2/colors?image_url=" + encodeURIComponent(imageUrl);
+
+const PORT = 5001 || 8081;
+
+app.use(cors());
+//This is middleware that allows use to send JSON requests
+// app.use(express.json());
+const fetch = async (url) => {
+    try {
+        const response = await got(url, {
+        username: apiKey,
+        password: apiSecret,
+        });
+        console.log(response.body);
+        return response.body;
+    } catch (error) {
+        console.log(error.response.body);
+    }
+};
+app.use("/colors", async (req, res) => {
+   
+    const url =
+      "https://api.imagga.com/v2/colors?image_url=" +
+      encodeURIComponent(req.query.url);
+    console.log(req.query.url);
+    const outPut = await fetch(url);
+    const colorAPI = JSON.parse(outPut);
+    console.log(`${colorAPI.result.colors.background_colors}`);
+
+    res.json(colorAPI);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
