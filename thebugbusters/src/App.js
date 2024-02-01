00cc6ddcaa5e4e2ef3a742a3c390a667887a4e6e
Sharon-SHH <sharonheng.shi@gmail.com>
2024-02-01 11:17:12 -0500

    get backend data

--- thebugbusters/src/App.js
+++ thebugbusters/src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 function App() {
+  const [colors, setColors] = useState(null);
+  
+  const getData = async ()=> {
+    try {
+      console.log(REACT_APP_SERVER_URL);
+      const response = await axios.get(`${REACT_APP_SERVER_URL}/profile`);
+      console.log(response);
+
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        {/* new line start*/}
+        <p>To get your profile details: </p>
+        <button onClick={getData}>Click me</button>
+        {colors && (
+          <div>
+            <p>Color Name: {colors.color_name}</p>
+            <p>Color Parent: {colors.color_parent}</p>
+          </div>
+        )}
+        {/* end of new line */}
+      </header>
     </div>
   );
 }
