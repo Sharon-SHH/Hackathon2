74c38b585f94ec179c9ab315bf8af156f7017af0
Sharon-SHH <sharonheng.shi@gmail.com>
2024-03-08 01:49:56 -0500

    layout

--- server/server.js
+++ server/server.js
@@ -19,29 +19,23 @@ const PORT = 5001 || 8081;
 
 app.use(cors());
 //This is middleware that allows use to send JSON requests
-// app.use(express.json());
 const fetch = async (url) => {
     try {
         const response = await got(url, {
         username: apiKey,
         password: apiSecret,
         });
-        console.log(response.body);
         return response.body;
     } catch (error) {
         console.log(error.response.body);
     }
 };
-app.use("/colors", async (req, res) => {
-   
+app.get("/colors", async (req, res) => {
     const url =
       "https://api.imagga.com/v2/colors?image_url=" +
       encodeURIComponent(req.query.url);
-    console.log(req.query.url);
     const outPut = await fetch(url);
     const colorAPI = JSON.parse(outPut);
-    console.log(`${colorAPI.result.colors.background_colors}`);
-
     res.json(colorAPI);
 });
 
